053ea28b5b44018183b2b362d75da5040e21cca6
Ednaldo Florenço <153869774+EdnaldoBarbo@users.noreply.github.com>
2024-09-05 23:25:23 +0000

    [feat]: Created a function for create table

--- draft.py
+++ draft.py
@@ -2,30 +2,32 @@ import pandas as pd
 import sqlite3
 
 from functions.create_db import create_db
+from functions.drop_table import drop_table
+from functions.create_table import create_table
 create_db ('mydatabase')
 
 conn = sqlite3.connect('mydatabase.db')
 cursor = conn.cursor()
 cursor.execute("""
         CREATE TABLE IF NOT EXISTS cliente (
-               id_cliente INT,
+               id_cliente INT PRIMARY KEY,
                nome TEXT
                )       
-               
                """)
 
 cursor.execute("""
      DROP TABLE cliente          
 """)
 
+# create table 
+columns_desc = """id_cliente INT PRIMARY KEY,
+    nome TEXT"""
 
-
-
-
-
-
-
-
+create_table(
+    database='mydatabase',
+    table_name='cliente',
+    columns_desc= columns_desc
+)
 
 
 
